e3cbc362c2fb28ff1bc4bd864f6db15bd488ce57
Juan David Berbeo Escobar <jberbeo@estratec.local>
2022-06-10 00:39:26 -0500

    creates form bsic css styles

---
+++ src/components/SueForm/SueFormContainer.tsx
@@ -0,0 +1,118 @@
+import React, {useState} from 'react'
+import { Container, Row, Form, Col, Button } from 'react-bootstrap/'
+import { useDispatch } from 'react-redux'
+import { postClaimAsync } from '../../store/features/claims/claimsSlice'
+
+export const SueFormContainer = () => {
+  const dispatch = useDispatch<any>()
+
+  const [newClaim, setNewClaim] = useState({
+      id: "",
+      documentMonth: "",
+      documentYear: "",
+      agreementDate: "",
+      claimerName: "",
+      claimerIDNumber: "",
+      claimerIDCity: "",
+      claimerCity: "",
+      claimerAddress: "",
+      claimerEmail: "",
+      defendantName: "",
+      facts:"",
+      proofs: ""
+  });
+  
+  const handleOnClick = (e: { preventDefault: () => void }) => {
+    e.preventDefault()
+    console.log('newClaim: ', newClaim)
+    dispatch(postClaimAsync({claimFields: newClaim}));
+  };
+
+  return (
+    <Container fluid style={{height: '100%'}}>
+      <Row style={{backgroundColor:'white', height: '30vh'}} className="align-items-center justify-content-center" sm={6}>
+            <Col sm={7} md={6} lg={6}>
+          <div>
+            <h2 style={{fontFamily: 'Raleway, sans-serif',  letterSpacing: '2px', textAlign: 'center', fontWeight: 800, color: '#EB646F', fontSize: '24px'}}>RECLAMACIÓN PRESTADOR DE SERVICIOS TURÍSTICOS</h2>
+          </div>
+            </Col>
+          </Row>
+          <Row className="justify-content-center" sm={6}>
+            <Col lg={10}>
+              <div className="h-75 d-inline-block">
+                <p style={{fontFamily: 'Raleway, sans-serif',  textAlign: 'left', fontWeight: 800, fontSize: '12px', marginBottom:'0px'}}>Estimado usuario.</p>
+                <p style={{fontFamily: 'Raleway, sans-serif',  textAlign: 'left', fontWeight: 500, fontSize: '10px', letterSpacing: '1px',}}>Tenga en cuenta que el presente formulario está diseñado para allegar reclamaciones por incumplimiento de prestadores turísticos ante la Superintendencia de Industria y Comercio - SIC</p>
+              </div>
+            </Col>
+      </Row>
+      <Row className="align-items-center justify-content-center" style={{backgroundColor:'#EB646F', height:'45vh'}}>
+      <Col sm={7} md={6} lg={5} >
+        <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Correo Electrónico</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" onChange={(e) => setNewClaim({ ...newClaim, claimerEmail: e.target.value })}/>
+        </Form.Group>
+        {/* <Form.Label>SELECCIONE EL TIPO DE PETICIÓN</Form.Label>
+        <Form.Select aria-label="Default select example" onChange={(e) => setNewClaim({ ...newClaim, id: e.target.value })}>
+          <option value="62a0203ab7ef665543db73f9">Retracto y Desistimiento</option>
+          <option value="62a1748b91f2dd1c91cd2140">Falta de Información al Consumidor</option>
+          <option value="62a1778b91f2dd1c91cd2142">Eximentes de la Responsabilidad</option>
+          <option value="62a2b59390feaadcf313fe7b">Publicidad Engañosa</option>
+        </Form.Select>  
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>NOMBRE</Form.Label>
+          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, claimerName: e.target.value })}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>NÚMERO DE IDENTIFICACIÓN *</Form.Label>
+          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, claimerIDNumber: e.target.value })}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>CIUDAD DEL DOCUMENTO DE IDENTIFICACIÓN</Form.Label>
+          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, claimerIDCity: e.target.value })}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>CELULAR*</Form.Label>
+          <Form.Control type="text"/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>CIUDAD DE DOMICILIO</Form.Label>
+          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, claimerAddress: e.target.value })}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>DIRECCIÓN</Form.Label>
+          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, claimerCity: e.target.value })}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Mes del documento</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" onChange={(e) => setNewClaim({ ...newClaim, documentMonth: e.target.value })}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>año del documento</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" onChange={(e) => setNewClaim({ ...newClaim, documentYear: e.target.value })}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Nombre del prestador o prestadores de servicios turísticos </Form.Label>
+          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, defendantName: e.target.value })}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword" >
+          <Form.Label>HECHOS</Form.Label>
+          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, facts: e.target.value })}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Fecha de la reclamación directa</Form.Label>
+          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, agreementDate: e.target.value })}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>AÑADIR PRUEBAS</Form.Label>
+          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, proofs: e.target.value })}/>
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={handleOnClick}>
+          Submit Form
+        </Button> */}
+        </Form>
+      </Col>
+    </Row>
+    </Container>
+  )
+}
--- src/views/SueForm.tsx
+++ src/views/SueForm.tsx
@@ -1,119 +1,14 @@
-import React, {useState} from 'react'
-import { Button, Form, Row, Col } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
 import { DefaultNavbar } from '../components/navbar/DefaultNavbar.tsx'
-import { getTodo, getTodoAsync } from '../store/features/todoSlice.js'
-import { postClaimAsync } from '../store/features/claims/claimsSlice'
-import { useEffect } from 'react'
+import { SueFormContainer } from '../components/SueForm/SueFormContainer.tsx'
+
 
 
 export const SueForm = () => {
-  const {hola} = useSelector((state) => {
-    return {
-      hola: state.hola.data
-    }
-  })
-  const dispatch = useDispatch<any>()
-
-  const [newClaim, setNewClaim] = useState({
-      id: "62a0203ab7ef665543db73f9",
-      documentMonth: "",
-      documentYear: "",
-      agreementDate: "",
-      claimerName: "",
-      claimerIDNumber: "",
-      claimerIDCity: "",
-      claimerCity: "",
-      claimerAddress: "",
-      claimerEmail: "",
-      defendantName: "",
-      facts:"",
-      proofs: ""
-  });
-
-  const getTodos = () => {
-    dispatch(getTodoAsync("5"))
-  }
-
-  useEffect(() => {
-  }, [])
-  
-  const handleOnClick = (e: { preventDefault: () => void }) => {
-    e.preventDefault()
-    console.log('newClaim: ', newClaim)
-    dispatch(postClaimAsync({claimFields: newClaim}));
-  };
-
   return (
     <>
     <DefaultNavbar />
-    <Row className="align-items-center justify-content-center">
-      <Col lg={6} >
-        <Form>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>NOMBRE</Form.Label>
-          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, claimerName: e.target.value })}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>NÚMERO DE IDENTIFICACIÓN *</Form.Label>
-          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, claimerIDNumber: e.target.value })}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>CIUDAD DEL DOCUMENTO DE IDENTIFICACIÓN</Form.Label>
-          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, claimerIDCity: e.target.value })}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Correo Electrónico</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" onChange={(e) => setNewClaim({ ...newClaim, claimerEmail: e.target.value })}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>CELULAR*</Form.Label>
-          <Form.Control type="text"/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>CIUDAD DE DOMICILIO</Form.Label>
-          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, claimerAddress: e.target.value })}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>DIRECCIÓN</Form.Label>
-          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, claimerCity: e.target.value })}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Mes del documento</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" onChange={(e) => setNewClaim({ ...newClaim, documentMonth: e.target.value })}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>año del documento</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" onChange={(e) => setNewClaim({ ...newClaim, documentYear: e.target.value })}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Nombre del prestador o prestadores de servicios turísticos </Form.Label>
-          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, defendantName: e.target.value })}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword" >
-          <Form.Label>HECHOS</Form.Label>
-          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, facts: e.target.value })}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Fecha de la reclamación directa</Form.Label>
-          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, agreementDate: e.target.value })}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>AÑADIR PRUEBAS</Form.Label>
-          <Form.Control type="text" onChange={(e) => setNewClaim({ ...newClaim, proofs: e.target.value })}/>
-        </Form.Group>
-        <Button variant="primary" type="submit" onClick={handleOnClick}>
-          Submit Form
-        </Button>
-        </Form>
-      </Col>
-    </Row>
-    <Button onClick={getTodos}>
-      get todos
-    </Button>
-    {hola.map((item) => {
-        return <p key={item.id}>{item.title}</p>;
-      })}
+    <SueFormContainer />
     </>
   )
 }
