12b9138d528819b5450101aa6205d23ce33c28d4
Juan David Berbeo Escobar <jberbeo@estratec.local>
2022-07-27 09:39:35 -0500

    feat: cleans code

--- src/views/SueForm.tsx
+++ src/views/SueForm.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { DefaultNavbar } from '../components/navbar/DefaultNavbar.tsx'
 import { SueFormContainer } from '../components/SueForm/SueFormContainer.tsx'
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 
 
 
