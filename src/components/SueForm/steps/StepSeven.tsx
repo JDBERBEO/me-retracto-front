22ee349d85ba2961f6c734482d34edef415f50d5
Juan David Berbeo Escobar <jberbeo@estratec.local>
2022-12-12 19:09:47 -0500

    fix: fix pay button

--- src/components/SueForm/steps/StepSeven.tsx
+++ src/components/SueForm/steps/StepSeven.tsx
@@ -1,20 +1,24 @@
 import React, { useEffect } from 'react';
-import { Col } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { getClaimsAsync } from '../../../store/features/claims/claimsSlice';
 import { ErrorHandler } from '../../common/ErrorHandler.tsx';
+import { Loader } from '../../common/spinner/Loader.tsx';
 
-export const StepSeven = () => {
+export const StepSeven = ({ i, goPreviousStep }) => {
   const dispatch = useDispatch();
-  const { previousCheckoutClaim, error } = useSelector((state: any) => state.claims);
+  const { previousCheckoutClaim, error, loading } = useSelector((state: any) => state.claims);
 
-  const checkout = new WidgetCheckout({
-    currency: 'COP',
-    amountInCents: parseFloat(previousCheckoutClaim.claimCreated.claimFields.casePrice) * 100,
-    reference: previousCheckoutClaim.claimCreated._id,
-    publicKey: process.env.REACT_APP_WOMPI_KEY,
-    redirectUrl: process.env.REACT_APP_REDIRECT_URL // Opcional
-  });
+  let checkout;
+  if (previousCheckoutClaim && previousCheckoutClaim.claimCreated !== undefined) {
+    checkout = new WidgetCheckout({
+      currency: 'COP',
+      amountInCents: parseFloat(previousCheckoutClaim.claimCreated.claimFields.casePrice) * 100,
+      reference: previousCheckoutClaim.claimCreated._id,
+      publicKey: process.env.REACT_APP_WOMPI_KEY,
+      redirectUrl: process.env.REACT_APP_REDIRECT_URL // Opcional
+    });
+  }
 
   const payment = (e) => {
     e.preventDefault();
@@ -26,20 +30,44 @@ export const StepSeven = () => {
     dispatch(getClaimsAsync());
   }, []);
 
-  if (error) {
-    return <ErrorHandler />;
+  if (loading) {
+    return (
+      <Row>
+        <Col>
+          <Row>
+            <Col
+              xs={12}
+              className="d-flex flex-column align-items-center justify-content-center"
+              style={{ height: '550px' }}>
+              <Loader variant={'success'} />
+              <p className="form-label mt-5">Cargando...</p>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    );
   }
 
   return (
-    <>
+    <div style={{ height: '500px' }}>
       <Col className="d-flex justify-content-center align-items-center flex-column" xs={12}>
         <p className="form-label py-5">Solo falta un paso para finalizar el proceso</p>
-        <div style={{ height: '250px' }}>
-          <button className="outlinedButton mb-5" onClick={payment}>
-            PAGAR
-          </button>
-        </div>
+        {/* <div style={{ height: '200px' }}> */}
+        {/* <button className="outlinedButton mb-5" onClick={payment}>
+          PAGAR
+        </button> */}
+        {/* </div> */}
+        {i === 0 ? null : (
+          <div>
+            <button className="previousStepbutton mb-5" onClick={goPreviousStep}>
+              {'ATRÁS'}
+            </button>
+            <button className="containerButton__size-m__green pd-3 mb-5" onClick={payment}>
+              PAGAR
+            </button>
+          </div>
+        )}
       </Col>
-    </>
+    </div>
   );
 };
