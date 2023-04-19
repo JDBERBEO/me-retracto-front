810ad299a890bc171841716302e621a8b6373c51
Juan David Berbeo <jd.berbeo@gmail.com>
2023-04-19 09:21:34 -0500

    fix: redesign footer, add contactUs text, add text to steps Container

--- src/components/common/footer/FooterMain.js
+++ src/components/common/footer/FooterMain.js
@@ -4,25 +4,28 @@ import { Col } from 'react-bootstrap';
 export const FooterMain = () => {
   return (
     <>
-      <Col className="d-flex justify-content-start align-items-center mt-5 mb-5" xs={12} md={4}>
+      <Col className="d-flex justify-content-center align-items-center mt-5 mb-5" xs={12} md={6}>
         <div>
-          <h5 className="sueCard_subtitle">Lorem ipsum dolor sit</h5>
-          <p className="sueCard_description">amet, consectetuer</p>
-          <p className="sueCard_description">adipiscing elit</p>
-          <p className="sueCard_description">adipiscing elit</p>
+          <h5 className="sueCard_subtitle">Politica de tratamiento de datos</h5>
+          <p className="sueCard_description">Cláusula de Responsabilidad</p>
+          <p className="sueCard_description"></p>
+          <p className="sueCard_description"></p>
         </div>
       </Col>
-      <Col className="d-flex justify-content-center align-items-center mt-5 mb-5" xs={12} md={4}>
+      <Col
+        className="d-flex justify-content-center align-items-center  text-align-right mt-5 mb-5"
+        xs={12}
+        md={6}>
         <div>
           <img
-            src="https://res.cloudinary.com/me-retracto/image/upload/v1667914984/platform%20Imgs/reducedLogo_jxkj1w.png"
+            src="https://res.cloudinary.com/me-retracto/image/upload/v1671555730/logo2_y4qhnd.png"
             alt="main-logo"
             className="defaultNavbar__logo"
           />
         </div>
       </Col>
-      <Col className="d-flex justify-content-end align-items-center text-align-right mt-5 mb-5">
-        <div style={{ textAlign: 'right' }}>
+      <Col className="d-flex justify-content-end align-items-center text-align-right" style={{}}>
+        <div style={{ textAlign: 'right', display: 'none' }}>
           <h5 className="sueCard_subtitle">Lorem ipsum dolor sit</h5>
           <p className="sueCard_description">amet, consectetuer</p>
           <p className="sueCard_description">adipiscing elit</p>
